c9e459e5009593de3db254a2a461063be049b949
simeonwillard <swillard4597@gmail.com>
2016-02-24 22:16:32 -0600

    learningfirst

--- git-commands.sh
+++ git-commands.sh
@@ -2,3 +2,12 @@ git clone https://github.com/simeonwillard/hello-world
 git add text.js
 git commit -m "message"
 git push origin master 
+
+git branch learningfirst
+git checkout learningfirst
+
+git fetch 
+git pull
+git status 
+dir 
+git log
